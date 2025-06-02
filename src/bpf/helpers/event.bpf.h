4306d29ad5eacbebb43737fb1edef7e82ad814bb
hassaanirshad <hassaanirshad88@gmail.com>
2025-06-02 22:59:54 +0000

    Add helper for tracepoint hook

--- src/bpf/helpers/event.bpf.h
+++ src/bpf/helpers/event.bpf.h
@@ -38,3 +38,21 @@ __ameba__##f_name(unsigned long long *ctx)				    \
 }	\
 typeof(__ameba__##f_name(0)) \
 BPF_PROG(__ameba__bpf__##f_name, ##args)
+
+
+#define AMEBA_HOOK_TP(sec_name, f_name, record_type, ctx_struct_type, ctx_arg_name) \
+__ameba__##f_name(ctx_struct_type ctx_arg_name); \
+static __always_inline typeof(__ameba__##f_name(0)) __ameba__bpf__##f_name(ctx_struct_type ctx_arg_name); \
+SEC(sec_name) \
+typeof(__ameba__##f_name(0)) \
+__ameba__##f_name(ctx_struct_type ctx_arg_name)				    \
+{									    \
+    struct event_context e_ctx; \
+    event_init_context(&e_ctx, record_type); \
+    if (!event_is_auditable(&e_ctx)) { \
+        return 0; \
+    } \
+	return __ameba__bpf__##f_name(ctx_arg_name);			    \
+}	\
+typeof(__ameba__##f_name(0)) \
+__ameba__bpf__##f_name(ctx_struct_type ctx_arg_name)
