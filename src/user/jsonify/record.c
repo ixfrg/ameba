900d4d3696cdd3f1e1dad62e8962c46ff38fde95
hassaanirshad <hassaanirshad88@gmail.com>
2025-06-11 21:46:50 +0000

    Add doc to jsonify/record

--- src/user/jsonify/record.c
+++ src/user/jsonify/record.c
@@ -2,7 +2,7 @@
 #include "user/error.h"
 
 
-int jsonify_record_connect(struct json_buffer *s, struct record_connect *data, int write_interpreted)
+static int jsonify_record_connect(struct json_buffer *s, struct record_connect *data, int write_interpreted)
 {
     int total = 0;
 
@@ -16,7 +16,7 @@ int jsonify_record_connect(struct json_buffer *s, struct record_connect *data, i
     return total;
 }
 
-int jsonify_record_accept(struct json_buffer *s, struct record_accept *data, int write_interpreted)
+static int jsonify_record_accept(struct json_buffer *s, struct record_accept *data, int write_interpreted)
 {
     int total = 0;
 
@@ -32,7 +32,7 @@ int jsonify_record_accept(struct json_buffer *s, struct record_accept *data, int
     return total;
 }
 
-int jsonify_record_namespace(struct json_buffer *s, struct record_namespace *data, int write_interpreted)
+static int jsonify_record_namespace(struct json_buffer *s, struct record_namespace *data, int write_interpreted)
 {
     int total = 0;
 
@@ -50,7 +50,7 @@ int jsonify_record_namespace(struct json_buffer *s, struct record_namespace *dat
     return total;
 }
 
-int jsonify_record_new_process(struct json_buffer *s, struct record_new_process *data, int write_interpreted)
+static int jsonify_record_new_process(struct json_buffer *s, struct record_new_process *data, int write_interpreted)
 {
     int total = 0;
 
@@ -64,7 +64,7 @@ int jsonify_record_new_process(struct json_buffer *s, struct record_new_process 
     return total;
 }
 
-int jsonify_record_cred(struct json_buffer *s, struct record_cred *data, int write_interpreted)
+static int jsonify_record_cred(struct json_buffer *s, struct record_cred *data, int write_interpreted)
 {
     int total = 0;
 
@@ -83,7 +83,7 @@ int jsonify_record_cred(struct json_buffer *s, struct record_cred *data, int wri
     return total;
 }
 
-int jsonify_record_send_recv(struct json_buffer *s, struct record_send_recv *data, int write_interpreted)
+static int jsonify_record_send_recv(struct json_buffer *s, struct record_send_recv *data, int write_interpreted)
 {
     int total = 0;
 
@@ -100,7 +100,7 @@ int jsonify_record_send_recv(struct json_buffer *s, struct record_send_recv *dat
     return total;
 }
 
-int jsonify_record_bind(struct json_buffer *s, struct record_bind *data, int write_interpreted)
+static int jsonify_record_bind(struct json_buffer *s, struct record_bind *data, int write_interpreted)
 {
     int total = 0;
 
@@ -113,7 +113,7 @@ int jsonify_record_bind(struct json_buffer *s, struct record_bind *data, int wri
     return total;
 }
 
-int jsonify_record_kill(struct json_buffer *s, struct record_kill *data, int write_interpreted)
+static int jsonify_record_kill(struct json_buffer *s, struct record_kill *data, int write_interpreted)
 {
     int total = 0;
 
@@ -126,7 +126,7 @@ int jsonify_record_kill(struct json_buffer *s, struct record_kill *data, int wri
     return total;
 }
 
-int jsonify_record_audit_log_exit(struct json_buffer *s, struct record_audit_log_exit *data, int write_interpreted)
+static int jsonify_record_audit_log_exit(struct json_buffer *s, struct record_audit_log_exit *data, int write_interpreted)
 {
     int total = 0;
 
--- src/user/jsonify/record.h
+++ src/user/jsonify/record.h
@@ -1,16 +1,23 @@
 #pragma once
 
+/*
+
+    A module to help write record types to json_buffer.
+
+    See 'core.h'.
+
+*/
 
 #include "user/jsonify/types.h"
 
+/*
+    Write record to json_buffer.
+    
+    e_common contains the record type. It is cast to record_* based on it's type to get the actual record.
 
-int jsonify_record_connect(struct json_buffer *s, struct record_connect *data, int write_interpreted);
-int jsonify_record_accept(struct json_buffer *s, struct record_accept *data, int write_interpreted);
-int jsonify_record_namespace(struct json_buffer *s, struct record_namespace *data, int write_interpreted);
-int jsonify_record_new_process(struct json_buffer *s, struct record_new_process *data, int write_interpreted);
-int jsonify_record_cred(struct json_buffer *s, struct record_cred *data, int write_interpreted);
-int jsonify_record_send_recv(struct json_buffer *s, struct record_send_recv *data, int write_interpreted);
-int jsonify_record_bind(struct json_buffer *s, struct record_bind *data, int write_interpreted);
-int jsonify_record_kill(struct json_buffer *s, struct record_kill *data, int write_interpreted);
-int jsonify_record_audit_log_exit(struct json_buffer *s, struct record_audit_log_exit *data, int write_interpreted);
+    Set 'write_interpreted' to non-zero value to interpret the record's contents like socket address.
+
+    Return:
+        See 'jsonify_core_snprintf'.
+*/
 int jsonify_record(struct json_buffer *s, struct elem_common *e_common, int data_len, int write_interpreted);
