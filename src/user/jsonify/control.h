630be422956ac126acb3c3608c5fc5c54afaa32b
hassaanirshad <hassaanirshad88@gmail.com>
2025-06-11 21:52:06 +0000

    Add doc to jsonify/control

--- src/user/jsonify/control.h
+++ src/user/jsonify/control.h
@@ -1,7 +1,21 @@
 #pragma once
 
+/*
+
+    A module to help write control_input to json_buffer.
+
+    See 'core.h'.
+
+*/
+
 #include "user/jsonify/core.h"
 #include "common/control.h"
 
 
+/*
+    Write control_input to json_buffer.
+
+    Return:
+        See 'jsonify_core_snprintf'.
+*/
 int jsonify_control_write_control_input(struct json_buffer *s, struct control_input *val);
