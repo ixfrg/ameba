d15f0e044a6a2855896ad6e49c159e30c1fca5a0
Hassaan Irshad <hassaanirshad88@gmail.com>
2025-07-09 22:32:49 +0000

    Update user arg tests

--- tests/user/args/user.cpp
+++ tests/user/args/user.cpp
@@ -34,50 +34,56 @@ TEST(UserArgUserInputGroup, TestParse)
     int argc = 0;
     char* argv[] = {};
     int res = user_args_user_must_parse_user_input(argc, argv);
-    CHECK_EQUAL(res, 0);
+    CHECK_EQUAL(0, res);
 }
 
 TEST(UserArgUserInputGroup, TestDefaults)
 {
+    struct user_input *u_in = &global_user_input;
+
     int argc = 0;
     char* argv[] = {};
     int res = user_args_user_must_parse_user_input(argc, argv);
-    CHECK_EQUAL(res, 0);
+    CHECK_EQUAL(0, res);
     
-    CHECK_EQUAL(global_user_input.c_in.lock, FREE);
-    CHECK_EQUAL(global_user_input.c_in.global_mode, IGNORE);
-    CHECK_EQUAL(global_user_input.c_in.uid_mode, IGNORE);
-    CHECK_EQUAL(global_user_input.c_in.uids_len, 0);
-    CHECK_EQUAL(global_user_input.c_in.pid_mode, IGNORE);
-    CHECK_EQUAL(global_user_input.c_in.pids_len, 0);
-    CHECK_EQUAL(global_user_input.c_in.ppid_mode, IGNORE);
-    CHECK_EQUAL(global_user_input.c_in.ppids_len, 0);
-    CHECK_EQUAL(global_user_input.c_in.netio_mode, IGNORE);
+    CHECK_EQUAL(FREE, u_in->c_in.lock);
+    CHECK_EQUAL(IGNORE, u_in->c_in.global_mode);
+    CHECK_EQUAL(IGNORE, u_in->c_in.uid_mode);
+    CHECK_EQUAL(0, u_in->c_in.uids_len);
+    CHECK_EQUAL(IGNORE, u_in->c_in.pid_mode);
+    CHECK_EQUAL(0, u_in->c_in.pids_len);
+    CHECK_EQUAL(IGNORE, u_in->c_in.ppid_mode);
+    CHECK_EQUAL(0, u_in->c_in.ppids_len);
+    CHECK_EQUAL(IGNORE, u_in->c_in.netio_mode);
 
-    CHECK_EQUAL(global_user_input.show_version, 0);
+    CHECK_EQUAL(0, u_in->show_version);
 
-    CHECK_EQUAL(global_user_input.o_type, OUTPUT_FILE);
+    CHECK_EQUAL(OUTPUT_FILE, u_in->o_type);
     STRNCMP_EQUAL(
-        global_user_input.output_file.path,
         "/tmp/current_prov_log.json",
+        u_in->output_file.path,
         strlen("/tmp/current_prov_log.json")
     );
-    CHECK_EQUAL(global_user_input.output_net.ip_family, 0);
-    CHECK_EQUAL(global_user_input.output_net.port, -1);
-    CHECK_EQUAL(global_user_input.output_net.ip[0], 0);
+    CHECK_EQUAL(0, u_in->output_net.ip_family);
+    CHECK_EQUAL(-1, u_in->output_net.port);
+    CHECK_EQUAL(0, u_in->output_net.ip[0]);
 }
 
 TEST(UserArgUserInputGroup, TestVersion)
 {
+    struct user_input *u_in = &global_user_input;
+
     int argc = 2;
     char* argv[] = {(char*)"test", (char*)"--version"};
     int res = user_args_user_must_parse_user_input(argc, argv);
-    CHECK_EQUAL(res, 0);
-    CHECK_EQUAL(global_user_input.show_version, 1);
+    CHECK_EQUAL(0, res);
+    CHECK_EQUAL(1, u_in->show_version);
 }
 
 TEST(UserArgUserInputGroup, TestOutputFile)
 {
+    struct user_input *u_in = &global_user_input;
+
     char* argv[] = {
         (char*)"test",
         (char*)"--file-path",
@@ -85,10 +91,14 @@ TEST(UserArgUserInputGroup, TestOutputFile)
     };
     int argc = sizeof(argv) / sizeof(char*);
     int res = user_args_user_must_parse_user_input(argc, argv);
-    CHECK_EQUAL(res, 0);
+    CHECK_EQUAL(0, res);
 
-    CHECK_EQUAL(global_user_input.o_type, OUTPUT_FILE);
-    STRNCMP_EQUAL(global_user_input.output_file.path, "/tmp/test.json", strlen("/tmp/test.json"));
+    CHECK_EQUAL(OUTPUT_FILE, u_in->o_type);
+    STRNCMP_EQUAL(
+        "/tmp/test.json",
+        u_in->output_file.path,
+        strlen("/tmp/test.json")
+    );
 }
 /*
 TEST(UserArgUserInputGroup, TestOutputNetValid)
@@ -101,10 +111,10 @@ TEST(UserArgUserInputGroup, TestOutputNetValid)
     int argc = sizeof(argv) / sizeof(char*);
     int res = user_args_user_must_parse_user_input(argc, argv);
     printf("res=%d\n", res);
-    CHECK_EQUAL(res, 0);
+    CHECK_EQUAL(0, res);
 
-    CHECK_EQUAL(global_user_input.o_type, OUTPUT_NET);
-    STRNCMP_EQUAL(global_user_input.output_net.ip, "0.0.0.0", strlen("0.0.0.0"));
+    CHECK_EQUAL(OUTPUT_NET, u_in->o_type);
+    STRNCMP_EQUAL("0.0.0.0", u_in->output_net.ip, strlen("0.0.0.0"));
 }
 */
 int main(int argc, char** argv)
